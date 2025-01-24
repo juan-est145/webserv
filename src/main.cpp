f8bd6056f81874843e87078a54a1278f92d7c096
juestrel <128648875+juan-est145@users.noreply.github.com>
2025-01-24 18:38:10 +0100

    Added debugging file

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,40 @@
 #include "../include/main.hpp"
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <string>
+#include <cstring>
 
 int main(void)
 {
-    std::cout << "Nothing yet LOL" << std::endl;
+    int serverFd = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverFd < 0)
+        return (-1);
+
+        
+    struct sockaddr_in address;
+    int addrLen = sizeof(address);
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = htonl(INADDR_ANY);
+    address.sin_port = htons(80);
+    
+    
+    int bindResult = bind(serverFd, (sockaddr *)&address, addrLen);
+    if (bindResult < 0)
+        return (-1);
+
+
+    if (listen(serverFd, 20) < 0)
+        return (-1);
+    while (1)
+    {
+        int newSocket = accept(serverFd, (sockaddr *)&address, (socklen_t *)&addrLen);
+        if (newSocket < 0)
+            return (-1);
+        std::string response = "Hola caracola";
+        write(newSocket, response.c_str(), response.size());
+        close(newSocket);
+    }
+    close(serverFd);
     return (0);
 }
