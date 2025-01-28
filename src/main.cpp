b732826aa38627998a849fe418f263397059e0d1
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2025-01-28 14:07:58 +0100

    Deleted unnecessary headers in main.cpp file

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,4 @@
 #include "../include/main.hpp"
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
-#include <string>
-#include <cstring>
 
 // TO DO: Check if port number is under 1024 and the bind process fails, send a message that says that with that port number, the server must run with root privileges.
 // 65535 is the maximum port number
