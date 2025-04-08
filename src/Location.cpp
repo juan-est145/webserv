096d802cefd8f58d2d7863a5be070d0c41147d8e
juestrel <128648875+juan-est145@users.noreply.github.com>
2025-04-08 17:56:42 +0200

    Added assignment operator to Location and setter for path

--- src/Location.cpp
+++ src/Location.cpp
@@ -32,7 +32,27 @@ namespace Webserv
 
     Location &Location::operator=(const Location &toCopy)
     {
-
+        if (this != &toCopy)
+        {
+            this->_path = toCopy._path;
+            this->_root = toCopy._root;
+            this->_autoindex = toCopy._autoindex;
+            this->_index = toCopy._index;
+            this->_return = toCopy._return;
+            this->_alias = toCopy._alias;
+            this->_clientMaxBodySize = toCopy._clientMaxBodySize;
+            this->_allowedMethods = toCopy._allowedMethods;
+            this->_cgiExt = toCopy._cgiExt;
+            this->_cgiPath = toCopy._cgiPath;
+        }
+        return (*this);
     }
+
+    void Location::setPath(std::string parameter)
+    {
+        this->_path = parameter;
+    }
+
+    Location::~Location() {}
 
 }
