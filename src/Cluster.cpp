4c1773b05960a4e51fd5166bc0eeeca58536cf2b
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2025-04-14 14:20:29 +0200

    Updated the new Server in Cluster.cpp

--- src/Cluster.cpp
+++ src/Cluster.cpp
@@ -6,7 +6,7 @@
 /*   By: juestrel <juestrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/12 16:24:38 by juestrel          #+#    #+#             */
-/*   Updated: 2025/04/14 14:01:33 by juestrel         ###   ########.fr       */
+/*   Updated: 2025/04/14 14:20:17 by juestrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -132,8 +132,7 @@ namespace Webserv
 				throw Cluster::ClusterException();
 			}
 			socketData.socketType = LISTEN_SOCKET;
-			// TO DO: Change the type of constructor
-			socketData.server = new Server();
+			socketData.server = new Server(it->second._configurations);
 			this->_sockets[listenFd] = socketData;
 			if (setsockopt(listenFd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &optVal, sizeof(optVal) == -1))
 			{
