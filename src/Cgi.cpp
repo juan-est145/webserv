e23603fe713232bf7bbc43842d0a0da0578681d4
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2025-05-04 21:18:40 +0200

    Added TO DO comment in Cgi for next day

--- src/Cgi.cpp
+++ src/Cgi.cpp
@@ -6,7 +6,7 @@
 /*   By: juestrel <juestrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/03 18:13:04 by juestrel          #+#    #+#             */
-/*   Updated: 2025/05/04 20:57:58 by juestrel         ###   ########.fr       */
+/*   Updated: 2025/05/04 21:18:24 by juestrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,6 +60,8 @@ namespace Webserv
 		if (indexes.first == -1 || indexes.second == -1)
 			return (false);
 		this->extractPathInfoAndInter(indexes, path, segmentedPath);
+		// TO DO: Map path to cgi script in local computer. Perhaps move mapPathToResource in ResourceReq
+		// to AuxFunc class
 		return (true);
 	}
 
