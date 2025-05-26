5fd27edad28a519ad146dc480466be8e20d23271
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2025-05-26 20:39:56 +0200

    Updated and deleted some cgi scripts

--- www/cgi/cgiForm.py
+++ www/cgi/cgiForm.py
@@ -5,8 +5,16 @@ import cgi
 form = cgi.FieldStorage()
 input = form.getvalue("input", "No value")
 
-print("<body>")
-print(" ")
-print(f"<h1>Your input was {input}</h1>")
-print(" ")
-print("</body>")
+message = f'''<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<h1>Your input was {input}</h1>
+</body>
+</html>'''
+
+print(message)
