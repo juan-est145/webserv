471df9b21a1eb1a93e485cecef068b8c2d3b8e28
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2025-05-26 20:37:38 +0200

    Updated the test.py script

--- www/cgi/test.py
+++ www/cgi/test.py
@@ -2,5 +2,17 @@
 
 import os
 
-print("<h1>Hello world</h1>")
-print(f"The HTTP_USER_AGENT env is {os.getenv('HTTP_USER_AGENT')}")
+message = f'''<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<h1>Hello world</h1>
+	<p>The HTTP_USER_AGENT env is {os.getenv('HTTP_USER_AGENT')}</p>
+</body>
+</html>'''
+
+print(message)
